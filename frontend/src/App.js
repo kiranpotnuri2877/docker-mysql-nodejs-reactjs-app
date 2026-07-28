8d6d412f947354003b784be1d9150d8de13bc876
kiranpotnuri2877 <kiranpotnuri287@gmail.com>
2026-07-28 19:02:33 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const URL = "http://localhost:3000";
+const URL = "";
 function App() {
   const [data, setData] = useState([]);
   const [inputValue, setInputValue] = useState("");
